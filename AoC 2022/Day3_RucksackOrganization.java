4a3200e123defa896e6045d732b710cd369bb81a
Nadine Kuo <hnkuo2@illinois.edu>
2022-12-04 10:18:13 -0600

    AOC 2022 day 3 puzzle 1

--- AoC 2022/Day3_RucksackOrganization.java
+++ AoC 2022/Day3_RucksackOrganization.java
@@ -11,22 +11,32 @@ public class Day3_RucksackOrganization {
     public static int getSumOfPriorities() throws FileNotFoundException {
 
         int sumOfPriorities = 0;
+        // Lowercase item types a through z have priorities 1 through 26
+        // Uppercase item types A through Z have priorities 27 through 52
+        final String alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
-        // Read input file, split each line in half
         URL path = Day3_RucksackOrganization.class.getResource("items_in_backpacks.txt");
         File file = new File(path.getFile());
         Scanner sc = new Scanner(file);
         while (sc.hasNext()) {
-            System.out.println(sc.nextLine());
+            // Read input file, split each line in half (assuming all lines have even no. of chars)
+            String itemsInTwoBackpacks = sc.nextLine();
+            int n = itemsInTwoBackpacks.length();
+            String left = itemsInTwoBackpacks.substring(0, n/2); // beginIdx is inclusive, endIdx is exclusive
+            String right = itemsInTwoBackpacks.substring(n/2, n);
+
+            // Check if there is any char that appears in both halves -> O(n^2)
+            for (int i = 0; i < left.length(); i++) {
+                char charToCheck = left.charAt(i);
+                if (right.indexOf(String.valueOf(charToCheck)) != -1) {
+                    // Duplicate char (item type) found --> get priority of charToCheck and add to sum
+                    int currPrior = alphabet.indexOf(String.valueOf(charToCheck)) + 1;
+                    System.out.println("Priority of " + charToCheck + " = " + currPrior);
+                    sumOfPriorities += currPrior;
+                    break;
+                }
+            }
         }
-
-        // Check if there is any char that appears in both halves
-
-        // Get priority for the char (type) that is duplicate
-
-
-        // Add to sum of priorities
-
         return sumOfPriorities;
     }
 }
