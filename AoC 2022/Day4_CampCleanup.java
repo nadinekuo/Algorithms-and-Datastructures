725356baa914afb382ed274739a66df359360fb2
Nadine Kuo <hnkuo2@illinois.edu>
2022-12-04 12:11:59 -0600

    AOC 2022 day 4 puzzle 2

--- AoC 2022/Day4_CampCleanup.java
+++ AoC 2022/Day4_CampCleanup.java
@@ -6,10 +6,11 @@ import java.net.URL;
 public class Day4_CampCleanup {
 
     public static void main(String[] args) throws FileNotFoundException {
-        System.out.println("Total no. of for which one range fully contains the other: " + getNumberOfPairs());
+        System.out.println("Total no. of pairs for which one range fully contains the other: " + getNumberOfPairsFullOverlap());
+        System.out.println("Total no. of pairs for which one range (partially) contains the other: " + getNumberOfPairsPartialOverlap());
     }
 
-    public static int getNumberOfPairs() throws FileNotFoundException {
+    public static int getNumberOfPairsFullOverlap() throws FileNotFoundException {
 
         int totalCount = 0;
 
@@ -38,6 +39,34 @@ public class Day4_CampCleanup {
                 if (start2 >= start1 && end2 <= end1) {
                     totalCount++;
                 }
+            }
+        }
+        return totalCount;
+    }
+
+    public static int getNumberOfPairsPartialOverlap() throws FileNotFoundException {
+
+        int totalCount = 0;
+
+        URL path = Day4_CampCleanup.class.getResource("cleaning_section_pairs.txt");
+        File file = new File(path.getFile());
+        Scanner sc = new Scanner(file).useDelimiter("[,\r\n]+"); // use "," and "\n" as delimiters
+
+        while (sc.hasNext()) {
+            // Each line represents ranges for 1 pair (2 elves), separated by comma
+            String[] sectionsElf1 = sc.next().split("-");
+            String[] sectionsElf2 = sc.next().split("-");
+//            System.out.println(Arrays.toString(sectionsElf1));
+//            System.out.println(Arrays.toString(sectionsElf2));
+
+            int start1 = Integer.parseInt(sectionsElf1[0]);
+            int end1 = Integer.parseInt(sectionsElf1[1]);
+            int start2 = Integer.parseInt(sectionsElf2[0]);
+            int end2 = Integer.parseInt(sectionsElf2[1]);
+
+            // Check if ranges overlap
+            if (start1 <= end2 && end1 >= start2) {
+                totalCount++;
             }
         }
         return totalCount;
